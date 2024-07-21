e211f22f5d91c2363899a4af68c0234eac85dfda
Maksym Maksimov <maksymmaksimov88@gmail.com>
2024-07-21 11:45:19 +0800

    Created Flask classifier app

---
+++ app.py
@@ -0,0 +1,64 @@
+import os
+import tensorflow as tf
+from flask import (
+    Flask,
+    flash,
+    request,
+    redirect,
+    render_template,
+)
+from werkzeug.utils import secure_filename
+from dotenv import load_dotenv
+
+from classifier import classifier
+
+
+load_dotenv()
+
+STATIC_FOLDER = "static"
+UPLOAD_FOLDER = os.path.join("static", "uploads")
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
+hcp_model = tf.keras.models.load_model(STATIC_FOLDER + "/models/" + "save_at_30.h5")
+
+# Initialize Flask app
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+# Ensure the upload folder exists
+if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+    os.makedirs(app.config["UPLOAD_FOLDER"])
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+
+        if file.filename == "":
+            flash("No selected file, please select file")
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            upload_image_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            file.save(upload_image_path)
+
+            predicted_label = classifier(hcp_model, upload_image_path)
+            return render_template(
+                "result.html", image=f"uploads/{filename}", prediction=predicted_label
+            )
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ classifier.py
@@ -0,0 +1,32 @@
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+
+
+# TARGET_SIZE = (300, 233)
+TARGET_SIZE = (200, 156)
+THRESHOLD = 0.5
+
+
+def load_and_preprocess_image(img_path):
+    img = load_img(img_path, target_size=TARGET_SIZE)
+    img_array = img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0
+    return img_array
+
+
+def classifier(model, img_path):
+    processed_image = load_and_preprocess_image(img_path)
+
+    prediction = model.predict(processed_image)
+
+    predicted_prob = prediction[0][0] * 100
+
+    predicted_label = (
+        f"'Savory': {100 - predicted_prob:.2f}% & 'Unsavory': {predicted_prob:.2f}%"
+    )
+
+    return predicted_label
+# import secrets
+#
+# print(secrets.token_hex(16))
